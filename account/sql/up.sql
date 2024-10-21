4f3576326d2364e1e15105b46e6bd00651815865
Rohit Vilas Ingole <rohit.vilas.ingole@gmail.com>
2024-10-21 10:46:54 +0530

    feature: account sql queries added

---
+++ account/sql/up.sql
@@ -0,0 +1,21 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE accounts (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_updated_at BEFORE
+UPDATE ON accounts FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column ();
