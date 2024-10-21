275a61cbfdbfdf1682160cba78daef44db3a31f7
Rohit Vilas Ingole <rohit.vilas.ingole@gmail.com>
2024-10-21 10:29:01 +0530

    feature: account service operations added

---
+++ account/service.go
@@ -0,0 +1,59 @@
+package account
+
+import (
+	"context"
+	"fmt"
+)
+
+type AccountService interface {
+	CreateAccount(ctx context.Context, email, name string) (*Account, error)
+	GetAccountByID(ctx context.Context, id string) (*Account, error)
+	GetAccountByEmail(ctx context.Context, email string) (*Account, error)
+	ListAccounts(ctx context.Context, limit, offset int) ([]Account, error)
+}
+
+type accountService struct {
+	repository AccountRepository
+}
+
+func NewAccountService(repository AccountRepository) (AccountService, error) {
+	return &accountService{repository: repository}, nil
+}
+
+func (service *accountService) CreateAccount(ctx context.Context, email, name string) (*Account, error) {
+	if err := service.repository.CreateAccount(ctx, email, name); err != nil {
+		return nil, fmt.Errorf("failed to create account: %w", err)
+	}
+
+	var account Account
+	account, err := service.repository.GetAccountByEmail(ctx, email)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get account: %w", err)
+	}
+
+	return &account, nil
+}
+
+func (service *accountService) GetAccountByID(ctx context.Context, id string) (*Account, error) {
+	account, err := service.repository.GetAccountByID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return &account, nil
+}
+
+func (service *accountService) GetAccountByEmail(ctx context.Context, email string) (*Account, error) {
+	account, err := service.repository.GetAccountByEmail(ctx, email)
+	if err != nil {
+		return nil, err
+	}
+	return &account, nil
+}
+
+func (service *accountService) ListAccounts(ctx context.Context, limit, offset int) ([]Account, error) {
+	accounts, err := service.repository.ListAccounts(ctx, limit, offset)
+	if err != nil {
+		return nil, err
+	}
+	return accounts, nil
+}
