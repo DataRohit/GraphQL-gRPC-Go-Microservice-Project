2973e77d7ccb4a4e315eb3adec9eab366cc47555
Rohit Vilas Ingole <rohit.vilas.ingole@gmail.com>
2024-10-21 18:25:38 +0530

    feature: gateway graphql server code added

---
+++ gateway/graph.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"graphql-grpc-go-microservice-project/account"
+)
+
+type GatewayServer struct {
+	AccountClient *account.AccountClient
+}
+
+func NewGraphQLServer(accountServiceURL string, secure bool) (*GatewayServer, error) {
+	accountClient, err := account.NewAccountClient(accountServiceURL, secure)
+	if err != nil {
+		return nil, err
+	}
+
+	return &GatewayServer{
+		AccountClient: accountClient,
+	}, nil
+}
